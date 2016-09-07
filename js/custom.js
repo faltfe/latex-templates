8b1dab1024deebf759ad45d08f562e993ea59d7c
Felix Faltin <felix.faltin@dnw.aero>
2016-09-07 12:33:44 +0200

    Initialize website

---
+++ js/custom.js
@@ -0,0 +1,29 @@
+$.fn.isOnScreen = function(){
+
+    var win = $(window);
+
+    var viewport = {
+        top : win.scrollTop(),
+        left : win.scrollLeft()
+    };
+    viewport.right = viewport.left + win.width();
+    viewport.bottom = viewport.top + win.height();
+
+    var bounds = this.offset();
+    bounds.right = bounds.left + this.outerWidth();
+    bounds.bottom = bounds.top + this.outerHeight();
+
+    return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
+};
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        $('section').each(function(idx){
+            if($(this).isOnScreen()) {
+                console.log('In View: ' + $(this).attr('class'));
+            } else {
+                console.log('Not In View: ' + $(this).attr('class'));
+            }
+        });
+    });
+});
